587dc171f9371ceb912dca16e00bb2989c576e55
Aditya raj <adityaraj99106@gmail.com>
2025-02-16 15:17:54 +0530

    added .env

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 ConnectDb()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(port, () => {
       console.log(`server is running at ${port}`);
     });
 
